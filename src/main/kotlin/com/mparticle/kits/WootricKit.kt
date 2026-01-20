19dfe324741c0c6262e184398133970627359d00
Mansi Pandya <mpandya@mparticle.com>
2026-01-20 14:22:59 -0500

    Fix lint error

--- src/main/kotlin/com/mparticle/kits/WootricKit.kt
+++ src/main/kotlin/com/mparticle/kits/WootricKit.kt
@@ -5,7 +5,9 @@ import com.mparticle.MParticle.IdentityType
 import com.mparticle.kits.KitIntegration.AttributeListener
 import com.wootric.androidsdk.Wootric
 
-class WootricKit : KitIntegration(), AttributeListener {
+class WootricKit :
+    KitIntegration(),
+    AttributeListener {
     private var endUserEmail: String? = null
     private var endUserCustomerId: String? = null
     private var endUserProperties: HashMap<String, String?>? = null
@@ -16,11 +18,12 @@ class WootricKit : KitIntegration(), AttributeListener {
         if (activityWeakReference != null) {
             val activity = activityWeakReference.get()
             if (activity != null) {
-                wootric = Wootric.init(
-                    activity,
-                    settings[CLIENT_ID],
-                    settings[ACCOUNT_TOKEN]
-                )
+                wootric =
+                    Wootric.init(
+                        activity,
+                        settings[CLIENT_ID],
+                        settings[ACCOUNT_TOKEN],
+                    )
                 wootric?.setProperties(endUserProperties)
                 setWootricIdentity(wootric)
             }
@@ -33,21 +36,26 @@ class WootricKit : KitIntegration(), AttributeListener {
 
     override fun onKitCreate(
         settings: Map<String, String>,
-        context: Context
+        context: Context,
     ): List<ReportingMessage> {
-        //it's important the Wootric is not initialized until the hosting app calls getInstance, with
-        //the correct Activity
+        // it's important the Wootric is not initialized until the hosting app calls getInstance, with
+        // the correct Activity
         require(
-            !(KitUtils.isEmpty(settings[CLIENT_ID]) ||
+            !(
+                KitUtils.isEmpty(settings[CLIENT_ID]) ||
                     KitUtils.isEmpty(CLIENT_SECRET) ||
-                    KitUtils.isEmpty(ACCOUNT_TOKEN))
+                    KitUtils.isEmpty(ACCOUNT_TOKEN)
+            ),
         ) { WOOTRIC_MISSING_REQUIRED_SETTINGS_MESSAGE }
         return emptyList()
     }
 
     override fun setOptOut(optedOut: Boolean): List<ReportingMessage> = emptyList()
 
-    override fun setUserIdentity(identityType: IdentityType, id: String) {
+    override fun setUserIdentity(
+        identityType: IdentityType,
+        id: String,
+    ) {
         if (IdentityType.Email == identityType) {
             endUserEmail = id
         } else if (IdentityType.CustomerId == identityType) {
@@ -77,19 +85,26 @@ class WootricKit : KitIntegration(), AttributeListener {
         wootric?.setEndUserEmail(endUserIdentifier)
     }
 
-    override fun setUserAttribute(key: String, value: String) {
+    override fun setUserAttribute(
+        key: String,
+        value: String,
+    ) {
         prepareEndUserProperties(key, value)
         if (wootric != null) {
             wootric?.setProperties(endUserProperties)
         }
     }
 
-    override fun setUserAttributeList(s: String, list: List<String>) {}
+    override fun setUserAttributeList(
+        s: String,
+        list: List<String>,
+    ) {}
+
     override fun supportsAttributeLists(): Boolean = false
 
     override fun setAllUserAttributes(
         attributes: Map<String, String>,
-        attributeLists: Map<String, List<String>>
+        attributeLists: Map<String, List<String>>,
     ) {
         endUserProperties = HashMap()
         for ((key, value) in attributes) {
@@ -101,17 +116,22 @@ class WootricKit : KitIntegration(), AttributeListener {
     }
 
     override fun removeUserAttribute(key: String) {
-        if (wootric != null && endUserProperties != null && endUserProperties?.remove(
+        if (wootric != null &&
+            endUserProperties != null &&
+            endUserProperties?.remove(
                 KitUtils.sanitizeAttributeKey(
-                    key
-                )
+                    key,
+                ),
             ) != null
         ) {
             wootric?.setProperties(endUserProperties)
         }
     }
 
-    private fun prepareEndUserProperties(key: String, value: String) {
+    private fun prepareEndUserProperties(
+        key: String,
+        value: String,
+    ) {
         endUserProperties?.let { it[KitUtils.sanitizeAttributeKey(key)] = value }
         if (endUserProperties == null) {
             endUserProperties = HashMap()
